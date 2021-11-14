7d63aa974d0558bf36797f51cb04aaf261ce29f0
maciejgieroba <maciej.gieroba@gmail.com>
2021-11-14 14:39:29 +0100

    Nawigation drawer works but is without animation and icon on left upper side

--- app/src/main/java/com/example/sharelist/HomeActivity.java
+++ app/src/main/java/com/example/sharelist/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.sharelist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,6 +19,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         btnLogout = findViewById(R.id.buttonLogout);
 
