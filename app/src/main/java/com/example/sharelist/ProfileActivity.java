503f3c9712bcd3b95dc6c95d047ac3fb35ab7234
sharqi12 <maciej.gieroba@gmail.com>
2021-11-30 22:09:00 +0100

    New profile page

--- app/src/main/java/com/example/sharelist/ProfileActivity.java
+++ app/src/main/java/com/example/sharelist/ProfileActivity.java
@@ -1,20 +1,45 @@
 package com.example.sharelist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileActivity extends Fragment {
+
+    TextView userEmail, changePasswordTV, userListsTV;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_profile, container , false);
+        super.onCreate(savedInstanceState);
 
+        View view = inflater.inflate(R.layout.activity_profile, container , false);
+        userEmail = view.findViewById(R.id.user_profile_name);
+        userEmail.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+        userListsTV = view.findViewById(R.id.user_profile_lists);
+        userListsTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListsActivity listsActivity = new ListsActivity();
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("dialog",false);
+                listsActivity.setArguments(bundle);
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, listsActivity).commit();
+            }
+        });
+
+
+
+        return view;
     }
 }
